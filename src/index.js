5a050a7fabb140f6870101843c9d2fef9ea107cc
Mariya Krasteva <104423761+MariyaMaki@users.noreply.github.com>
2022-06-28 09:45:45 +0200

    add search button

--- src/index.js
+++ src/index.js
@@ -1,39 +1,52 @@
-const testWp = async() =>{
-    
-    const response = await fetch('https://character-database.becode.xyz/characters');
+const testWp = async () => {
+  const response = await fetch(
+    "https://character-database.becode.xyz/characters"
+  );
 
-    const json = await response.json().catch(console.error);
+  const json = await response.json().catch(console.error);
 
-    console.log(json)
+  console.log(json);
 
-        for(let i=0; i < json.length;i++){
+  for (let i = 0; i < json.length; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("id", "div" + i);
+    div.setAttribute("class", "div_test");
+    document.getElementById("list_char").appendChild(div);
 
-            const div = document.createElement("div");
-                div.setAttribute("id","div"+i);
-                div.setAttribute("class","div_test");
-                document.getElementById('list_char').appendChild(div);
+    const img = document.createElement("img");
+    img.setAttribute("id", "pImg" + i);
+    img.setAttribute("src", "data:image/png;base64," + json[i].image);
+    document.getElementById("div" + i).appendChild(img);
 
-                const img = document.createElement("img");
-                img.setAttribute("id","pImg"+i);
-                img.setAttribute("src","data:image/png;base64,"+json[i].image);
-                document.getElementById("div"+i).appendChild(img);
-        
-                const pName = document.createElement("p");
-                pName.setAttribute("id","pName"+i);
-                pName.setAttribute("class","p_test_name");
-                document.getElementById("div"+i).appendChild(pName);
-        
-                document.getElementById("pName"+i).innerHTML = json[i].name;
-        
-                const pDesc = document.createElement("p");
-                pDesc.setAttribute("id","pDesc"+i);
-                pDesc.setAttribute("class","p_test_desc");
-                document.getElementById("div"+i).appendChild(pDesc);
-        
-                document.getElementById("pDesc"+i).innerHTML = json[i].shortDescription;
-        }
-}
+    const pName = document.createElement("p");
+    pName.setAttribute("id", "pName" + i);
+    pName.setAttribute("class", "p_test_name");
+    document.getElementById("div" + i).appendChild(pName);
 
-window.addEventListener('load', function (e) {
-    testWp();
+    document.getElementById("pName" + i).innerHTML = json[i].name;
+
+    const pDesc = document.createElement("p");
+    pDesc.setAttribute("id", "pDesc" + i);
+    pDesc.setAttribute("class", "p_test_desc");
+    document.getElementById("div" + i).appendChild(pDesc);
+
+    document.getElementById("pDesc" + i).innerHTML = json[i].shortDescription;
+  }
+};
+
+window.addEventListener("load", function (e) {
+  testWp();
 });
+// const f = document.getElementById('form');
+// const q = document.getElementById('query');
+// const google = 'https://www.google.com/search?q=site%3A+';
+// const site = 'pagedart.com';
+
+// function submitted(event) {
+//   event.preventDefault();
+//   const url = google + site + '+' + q.value;
+//   const win = window.open(url, '_blank');
+//   win.focus();
+// }
+
+// f.addEventListener('submit', submitted);
