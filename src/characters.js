8ad85dc793085ed98a78169cadbc1900f921e1e0
kingdragox99 <kingdragox99@gmail.com>
2022-06-28 15:50:54 +0200

    add new code

---
+++ src/characters.js
@@ -0,0 +1,3 @@
+export function character(a){
+    return document.write('<!DOCTYPE html><html><head><title>'+a+'</title><script type="module" src="./src/index.js"></script><link rel="stylesheet" href="./src/css/index.min.css" /></head><body><header></header><main><h1 class="h1_char" id="h1_char">'+a+'</h1></main><footer></footer></body></html>');
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const listCharacters = async () => {
-  const response = await fetch(
-    "https://character-database.becode.xyz/characters"
-  );
+// fetch api + making card for each characters
 
-  const json = await response.json().catch(console.error);
-
-  console.log(json);
+const listCharacters = async() => {
+    const response = await fetch(
+      "https://character-database.becode.xyz/characters"
+    );
+  
+    const json = await response.json().catch(console.error);
 
   for (let i = 0; i < json.length; i++) {
     const div = document.createElement("div");
@@ -32,21 +32,60 @@ const listCharacters = async () => {
     document.getElementById("div" + i).appendChild(pDesc);
 
     document.getElementById("pDesc" + i).innerHTML = json[i].shortDescription;
+
+    const aBtn = document.createElement("a");
+    aBtn.setAttribute("id", "aBtn" + i);
+    aBtn.setAttribute("class", "p_test_btn");
+    aBtn.setAttribute("href","yooooo")
+    document.getElementById("div" + i).appendChild(aBtn);
+
+    document.getElementById("aBtn" + i).innerHTML = "Learn More";
+
   }
 };
 
+// Making page for characters with info, do i need to refetch ?
+
 const f = document.getElementById('form');
 const q = document.getElementById('query');
-const site = 'https://character-database.becode.xyz/characters/?name=';
+const site = 'http://127.0.0.1:5500/';
 
-function submitted(event) {
+const submitted = async(event) => {
+
         event.preventDefault();
         const url = site + q.value;
-        const win = window.open(url, '_blank');
-        win.focus();
- }
+        window.open(url,"_self").document.write('<!DOCTYPE html><html><head><title>'+q.value+'</title><script type="module" src="./src/index.js"></script><link rel="stylesheet" href="./src/css/index.min.css" /></head><body><header><h1 class="h1_char" id="h1_char">'+q.value+'</h1></header><main><div class="main_char_div" id="main_char_div"></div></main><footer></footer></body></html>');
+            
+        const response = await fetch(
+            "https://character-database.becode.xyz/characters?name="+q.value
+        );
+    
+        const json = await response.json().catch(console.error);
+        
+        const h1 = document.getElementById("h1_char").innerHTML = json[0].name;
 
- f.addEventListener('submit', submitted);
- window.addEventListener("load", function (e) {
+        const img = document.createElement("img");
+            img.setAttribute("id", "pImg");
+            img.setAttribute("src", "data:image/png;base64," + json[0].image);
+            img.setAttribute("class", "img_char")
+            
+            document.getElementById("main_char_div").appendChild(img);
+
+        const pDeskL = document.createElement("p");
+            pDeskL.setAttribute("id", "pName");
+            pDeskL.setAttribute("class", "p_char_desc");
+            document.getElementById("main_char_div").appendChild(pDeskL);
+
+            document.getElementById("pName").innerHTML = json[0].description;
+
+            console.log(json)
+
+    }
+
+// event listener but look weardo
+
+window.addEventListener("load", function (e) {
     listCharacters();
 });
+
+f.addEventListener('submit', submitted);
