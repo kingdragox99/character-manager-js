8bbecf10af9fd25041ed2facb3ef4607d2b6566c
kingdragox99 <kingdragox99@gmail.com>
2022-06-29 09:32:36 +0200

    voila

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
-
 module.exports = {
   mode: "production", // "production" | "development" | "none"
   // Chosen mode tells webpack to use its built-in optimizations accordingly.
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [{
+            loader: "sass-loader",
+            options: {
+                minimize: true
+            }
+        }]
+    }]
+}
 };
