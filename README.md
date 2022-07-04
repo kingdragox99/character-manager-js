b7a7634091aad3e9e2b3af2745e934ad2352d94a
kingdragox99 <kingdragox99@gmail.com>
2022-07-04 15:59:10 +0200

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# character-manager-js
+
+# Character manager
+
+Making a website that shows a list of characters and alows the user to interact with it through different features.
+
+
+## Acknowledgements
+
+ - [Character manager](https://kingdragox99.github.io/character-manager-js/)
+
+
+## Authors
+
+js : 
+- [Arthur Lefevre](https://github.com/kingdragox99)
+Sass html css:
+- [Mariya Krasteva](https://github.com/MariyaMaki)
+
+
+## 🛠 Skills
+Javascript, HTML, CSS, sass...
+
+
+## Features
+
+- Add characters
+- Edit characters
+- Delete characters
+- Search bar
+## To do
+
+- Fix bug on img
+## Screenshots
+[![Character-Manager.png](https://i.postimg.cc/nhtt5WJw/Character-Manager.png)](https://postimg.cc/jLv9nZCz)
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); //to access built-in plugins
+
 module.exports = {
+  mode: 'development',
   mode: "production", // "production" | "development" | "none"
   // Chosen mode tells webpack to use its built-in optimizations accordingly.
   entry: {
     home: './src/index.js',
-    maker: './src/characters.js',
-    modif: './src/modifcharacters.js',
+    edit: './src/modifcharacters.js',
+    add: './src/characters',
   },
   output: {
     path: __dirname + '/dist',
     filename: "[name].bundle.js"
   },
   module: {
-    rules: [{
-      test: /\.s[ac]ss$/i,
-      use: [
-        // Creates `style` nodes from JS strings
-        "style-loader",
-        // Translates CSS into CommonJS
-        "css-loader",
-        // Compiles Sass to CSS
-        "sass-loader",
-      ],
-    }]
-  }
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+      chunks: ['home'],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'modif.html',
+      template: './modif.html',
+      chunks: ['edit'],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'pageAjt.html',
+      template: './pageAjt.html',
+      chunks: ['add'],
+    }),
+  ],
 };
